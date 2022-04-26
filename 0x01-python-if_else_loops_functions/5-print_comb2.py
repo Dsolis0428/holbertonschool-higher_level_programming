c4f29ece35c29ed4c4b2438d3f2629d1d7f1e38c
Dylan Solis <dylansolis0428@gmail.com>
2022-04-26 20:05:41 +0000

    corrected display format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number < 10:
-        print("0{}, ".format(number))
+        print("0{}".format(number), end=", ")
     else:
-        print("{}, ".format(number))
+        print("{}".format(number), end=", ")
