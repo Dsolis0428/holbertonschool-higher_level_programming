6a0c5191efad6614bc9b50b83a59a31afd41af7e
Dylan Solis <dylansolis0428@gmail.com>
2022-04-26 22:25:35 +0000

    addinf 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """function that prints the numbers from 1 to 100 separated by a space."""
+    for number in range(0, 100):
+        if number % 3 == 0 and number % 5 == 0
+        print("FizzBuzz ", end="")
+    elif number % 3 == 0:
+        print("Fizz ", end="")
+    elif number % 5 == 0:
+        print("buzz ", end="")
+    else:
+        print("{} ".format(number), end="")
