0fde58dc8029d3e50c1dda2408bd41ecd69931fe
Dylan Solis <dylansolis0428@gmail.com>
2022-04-26 22:03:36 +0000

    variable correction

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     if number < 0:
         number *= -1
     digit = number % 10
-    print({:d}".format(last_digit), end="")
+    print({:d}".format(digit), end="")
     return (digit)
