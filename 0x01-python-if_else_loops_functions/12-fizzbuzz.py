739881556fe58f1b813ec5512fada67a3cc7b7b9
Dylan Solis <dylansolis0428@gmail.com>
2022-04-26 22:31:32 +0000

    fixing range

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     """function that prints the numbers from 1 to 100 separated by a space."""
-    for number in range(0, 100):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         elif number % 3 == 0:
