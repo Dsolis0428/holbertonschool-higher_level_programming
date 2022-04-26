0652f1607621991a8cb1decbf9852c3489219256
Dylan Solis <dylansolis0428@gmail.com>
2022-04-26 22:29:56 +0000

    indentation fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@ def fizzbuzz():
     """function that prints the numbers from 1 to 100 separated by a space."""
     for number in range(0, 100):
         if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz ", end="")
-    elif number % 3 == 0:
-        print("Fizz ", end="")
-    elif number % 5 == 0:
-        print("buzz ", end="")
-    else:
-        print("{} ".format(number), end="")
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
