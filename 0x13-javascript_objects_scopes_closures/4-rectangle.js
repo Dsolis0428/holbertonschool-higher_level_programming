91daec55daa1ea1bbca743bc1644d9716eb780b1
Dylan <dylansolis0428@gmail.com>
2022-08-02 22:34:14 +0000

    task 4 fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,13 +12,15 @@ module.exports = class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+	
    rotate () {
 	   const exhange = this.width;
 	   this.width = this.height;
 	   this.height = exchange;
    }
+
    double () {
 	   this.width *= 2;
 	   this.hieght *= 2;
    }
-}
+};
