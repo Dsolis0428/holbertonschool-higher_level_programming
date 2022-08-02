1fbb3f23041a9ee77837a3b88948301315708288
Dylan <dylansolis0428@gmail.com>
2022-08-02 03:09:06 +0000

    making task 7executable

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
+
+const arg = process.argv[2];
+const argint = parseInt(arg);
+if (arg === 'undefined' || isNaN(argint)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < argint; i++) {
     console.log('C is fun');
   }
 }
